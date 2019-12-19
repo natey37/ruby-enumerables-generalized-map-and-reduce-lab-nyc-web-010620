798e4448ca192d1f4c57067c7bbe2cd8c962e4a9
Nathan Brickett <natey37@gmail.com>
2019-12-19 05:51:03 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # Your Code Here
+def map(array)
+  new_array = [] 
+  index = 0 
+  
+  while index < array.count do 
+    new_array.push(yield(array[index]))
+    index += 1 
+  end 
+  new_array
+end 
+
+def reduce(array, starting_value = nil)
+  
+  if starting_value
+    sum = starting_value
+    index = 0 
+  else 
+    sum = array[0]
+    index = 1 
+  end 
+   
+  while index < array.count do 
+    sum = yield(sum, array[index])
+    index += 1 
+  end 
+   sum 
+
+  
+end 
+  
